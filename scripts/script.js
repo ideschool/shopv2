e929a893dd2079f01910b33d19134e87086c8a3e
Tomasz Korenberg <info@casestudio.pl>
2019-12-17 14:54:31 +0100

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+//import Api from "./api";
+import Shop from "./shop";
+
+
+//const api = new Api();
+const shop = new Shop();
+
+shop();
+
+
+
+
+
+
---
+++ scripts/shop.js
@@ -0,0 +1,5 @@
+const Shop = function(){
+    console.log("test")
+};
+
+export default Shop;
