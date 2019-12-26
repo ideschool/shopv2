3c5e943c2d23109ab566ab323a3c3c1bc9f9dceb
Tomasz Korenberg <info@casestudio.pl>
2019-12-26 17:33:21 +0100

    Added: - reload product count after click "add to cart" - adding product to cart array after click "add to cart" - product name added

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 import Api from "./api.js";
 import Shop from "./shop.js";
+import Cart from "./cart.js";
 
 
 const api = new Api();
 const shop = new Shop();
+const cartShow = new Cart();
 
 api.getAll().then(res => shop.showAllProducts(res));
 
--- scripts/shop.js
+++ scripts/shop.js
@@ -1,21 +1,80 @@
 import Api from "./api.js";
-const api = new Api()
+
+const api = new Api();
 
 
-class Shop{
+class Shop {
 
-    reloadElement(data){
-        this.reload = document.querySelector("#product-" + data._id);
-        console.log("product-" + data._id);
-        return this.reload
+    constructor() {
+        this.cart = [];
+    }
+
+    buyAndReloadElement(data){
+
+        function reload (productId){
+            let reloadCount = ""
+            reloadCount = document.querySelector("#product-"+data._id+"-count-div")
+            api.getOne(productId).then(res => reloadCount.innerText = "Dostępna ilośc: " + res.result.data.count + " szt.")
+        }
+        api.buy(data._id, {count: document.querySelector("#add-to-cart-input" + data._id).value})
+            .then(res => reload(res.result.id))
+        //console.log(data);
+        //return this.reload
+    }
+
+    itemsInCartShowing(cart) {
+        this.numberOfItems = document.querySelector("#cart-items-number")
+        this.numberOfItems.innerText = cart.length
+    }
+
+    isProductInCart(data, cart) {
+        let isProduct = false;
+        if (cart.length === 0) {
+            //console.log("Koszyk jest pusty - dodaj pierwszy produkt")
+            return isProduct
+        } else {
+            if (cart.find(el => el.id === data._id) === undefined) {
+                //console.log("W koszyku nie ma tego produktu - dadaj produkt")
+                return isProduct
+            } else {
+                //console.log("W koszyku jest ten produkt - podmien ilośc")
+                return isProduct = true;
+            }
+        }
+    }
+
+    findProductIndexInCart(data, cart) {
+        this.productIndex = cart.findIndex(el => el.id === data._id)
+        //console.log(this.productIndex)
+        return this.productIndex
+    }
+
+
+    addToCart(data) {
+        if (this.isProductInCart(data, this.cart) === false) {
+            this.cart.push({
+                id: data._id,
+                count: document.querySelector("#add-to-cart-input" + data._id).value
+            })
+            this.itemsInCartShowing(this.cart)
+            this.buyAndReloadElement(data)
+
+        } else {
+            this.productIndex = this.findProductIndexInCart(data, this.cart)
+            this.cart[this.productIndex].count = parseInt(this.cart[this.productIndex].count) + parseInt(document.querySelector("#add-to-cart-input" + data._id).value)
+            this.buyAndReloadElement(data)
+        }
+
+
     }
 
     /**
      * Product detail list
      * @param one product data
      */
-    detailList(data){
+    detailList(data) {
         this.productDiv.appendChild(this.idDivBuilder(data));
+        this.productDiv.appendChild(this.nameDivBuilder(data));
         this.productDiv.appendChild(this.countDivBuilder(data));
         this.productDiv.appendChild(this.priceDivBuilder(data));
         this.productDiv.appendChild(this.addToCartDivBuilder(data));
@@ -25,18 +84,19 @@ class Shop{
      * @param all products data
      * @returns <div> with product details
      */
-    productDivBuilder(data){
+    productDivBuilder(data) {
         this.productDiv = '';
         this.productDiv = document.createElement("div");
         this.detailList(data)
         return this.productDiv
-    };
+    }
+    ;
 
     /**
      * @param one product data
      * @returns <div> with product ID
      */
-    idDivBuilder(data){
+    idDivBuilder(data) {
         this.idDiv = "";
         this.idDiv = document.createElement("div");
         this.idDiv.innerText = "ID: " + data._id;
@@ -46,13 +106,26 @@ class Shop{
 
     /**
      * @param one product data
+     * @returns <div> with product name
+     */
+    nameDivBuilder(data) {
+        this.nameDiv = "";
+        this.nameDiv = document.createElement("div");
+        this.nameDiv.innerText = "Nazwa: " + data.data.name;
+        this.nameDiv.className = "product-name-div"
+        return this.nameDiv;
+    }
+
+    /**
+     * @param one product data
      * @returns <div> with product count
      */
-    countDivBuilder(data){
+    countDivBuilder(data) {
         this.countDiv = "";
         this.countDiv = document.createElement("div");
         this.countDiv.innerText = "Dostępna ilośc: " + data.data.count + " szt.";
         this.countDiv.className = "product-count-div"
+        this.countDiv.id = "product-"+data._id+"-count-div"
         return this.countDiv;
     }
 
@@ -60,7 +133,7 @@ class Shop{
      * @param one product data
      * @returns <div> with product price
      */
-    priceDivBuilder(data){
+    priceDivBuilder(data) {
         this.priceDiv = "";
         this.priceDiv = document.createElement("div");
         this.priceDiv.innerText = "Cena: " + data.data.price + " zł";
@@ -71,7 +144,7 @@ class Shop{
     /**
      * @returns add to cart <input>
      */
-    addToCartInputBuilder(data){
+    addToCartInputBuilder(data) {
         this.addToCartInput = document.createElement("input");
         this.addToCartInput.type = "number";
         this.addToCartInput.step = "1";
@@ -85,17 +158,14 @@ class Shop{
      *
      * @returns add to cart <button>
      */
-    addToCartButtonBuilder(data){
+    addToCartButtonBuilder(data) {
 
         this.addToCartButton = document.createElement("button");
         this.addToCartButton.innerText = "Dodaj do koszyka";
         this.addToCartButton.className = "add-to-cart-button";
         this.addToCartButton.addEventListener("click", function () {
-            api.buy(data._id, {count: document.querySelector("#add-to-cart-input" + data._id).value})
-            //todo:poprawic funkcje reload. nie wiem czemu jej nie widzi, chuba jakis problem z zakresami
-                .then(this.reloadElement(data))
-            //console.log(data._id)
-            })
+            this.addToCart(data)
+        }.bind(this))
         return this.addToCartButton
     }
 
@@ -103,7 +173,7 @@ class Shop{
      * @param one product data
      * @returns <div> with numbers of product to cart & add to cart button
      */
-    addToCartDivBuilder(data){
+    addToCartDivBuilder(data) {
         this.addToCartDiv = "";
         this.addToCartDiv = document.createElement("div");
         this.addToCartDiv.appendChild(this.addToCartInputBuilder(data));
@@ -116,17 +186,17 @@ class Shop{
      * Show all products from database
      * @param all data from database
      */
-    showAllProducts(data){
+    showAllProducts(data) {
         this.allProductsDiv = "";
         this.allProductsDiv = document.querySelector("#all-products");
-        for (let i=0; i<data.length; i++) {
+        for (let i = 0; i < data.length; i++) {
             this.product = this.productDivBuilder(data[i]);
             this.product.id = "product-" + data[i]._id;
             this.product.className = "product-div";
             this.allProductsDiv.appendChild(this.product)
         }
-}
+    }
 
 }
-
+const shop = new Shop()
 export default Shop;
