5c2f0707a84cb02b78c75d16eeda4390383eb84b
ideschool <57493267+ideschool@users.noreply.github.com>
2019-12-28 15:32:08 +0100

    Update cart.js

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,21 +1,29 @@
 class Cart {
+    constructor() {
+        this.createModal();
+    }
     cartClick() {
         this.link = document.querySelector("#cart-items-link")
-        this.link.addEventListener("click", this.modal)
+        this.link.addEventListener("click", this.showModal.bind(this))
     }
-    modal(cartItems) {
+    createModal(cartItems) {
         this.cartModal = document.createElement("div");
         this.cartModal.className = "cart-modal";
         this.cartModal.innerHTML = "<p>" + "test test test test" + "</p>";
         this.closeButton = document.createElement("button");
         this.closeButton.innerText = "ⓧ";
         this.closeButton.className = "close-button";
-        this.closeButton.addEventListener("click", () => {
-            this.cartModal.remove()
-        });
+        this.closeButton.addEventListener("click", this.hideModal.bind(this));
         this.cartModal.appendChild(this.closeButton);
-        this.cartModal.appendChild(this.modalTable())
+        // Jaki sens ma ta metoda modalTable() skoro powyżej wykonujesz podobne czynności w ciele funkcji.
+        this.cartModal.appendChild(this.modalTable());
         document.documentElement.appendChild(this.cartModal);
+    }
+    showModal() {
+        this.cartModal().classList.append('show');
+    }
+    hideModal() {
+        this.cartModal().classList.remove('show');
     }
     modalTable() {
         this.table = document.createElement("table")
